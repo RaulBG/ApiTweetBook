a820466300faf9efed1ae9d5a10cb7362d414c76
Raul <raul.bullidog@gmail.com>
2022-02-07 11:41:23 +0100

    Create  a new class for Response and dont expon my domain

--- TweetBook/Controllers/v1/PostController.cs
+++ TweetBook/Controllers/v1/PostController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TweetBook.Contracts;
+using TweetBook.Controllers.v1.Requests;
+using TweetBook.Controllers.v1.Responses;
 using TweetBook.Domain;
 
 namespace TweetBook.Controllers.v1
@@ -28,17 +30,19 @@ namespace TweetBook.Controllers.v1
         }
 
         [HttpPost(ApiRoutes.Post.Create)]
-        public IActionResult Create([FromBody] Post post)
+        public IActionResult Create([FromBody] CreatePostRequest postRequest)
         {
+            Post post = new Post{Id= postRequest.Id}; // No se deben de mezclar los contratos de las apis con los objetos del dominoo
+
             if(string.IsNullOrEmpty(post.Id))
-            {
-                post.Id= Guid.NewGuid().ToString();
-            }
+                post.Id= Guid.NewGuid().ToString();            
 
             _postCollection.Add(post);
 
             var baseUrl=$"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.ToUriComponent()}";
             var locationUri= $"{baseUrl}/{ApiRoutes.Post.Get.Replace("{postId}",post.Id)}";
+
+            PostResponse response = new PostResponse{Id=post.Id};
 
             return Created(locationUri, post);
         }
---
+++ TweetBook/Controllers/v1/Responses/PostResponse.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TweetBook.Controllers.v1.Responses
+{
+    public class PostResponse
+    {
+        public string Id {get;set;}
+        
+    }
+}
